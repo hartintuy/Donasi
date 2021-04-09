875a00c1350bd0ecfa97aeafc3fc65c8a6ba769d
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-04-09 15:55:36 +0700

    merapikan code, tambah fungsi register dan add ke database

--- app/src/main/java/com/takatutustudio/Donasi/RegisterOneActivity.java
+++ app/src/main/java/com/takatutustudio/Donasi/RegisterOneActivity.java
@@ -3,15 +3,21 @@ package com.takatutustudio.Donasi;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,15 +26,15 @@ import com.google.firebase.database.ValueEventListener;
 
 public class RegisterOneActivity extends AppCompatActivity {
 
-    LinearLayout btn_back;
-    EditText username, password, email_address;
-    DatabaseReference reference, reference_username;
-    Button btn_continue;
+    LinearLayout btnBack;
+    EditText etUsername, etPassword, etEmailAddress;
+    DatabaseReference reference, refUsers;
+    Button btnContinue;
 
-    String USERNAME_KEY = "usernamekey";
-    String username_key = "";
+    String USERNAME_KEY = "username_key";
 
     private FirebaseAuth mAuth;
+    private FirebaseDatabase mFirebaseDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,86 +42,101 @@ public class RegisterOneActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register_one);
 
         mAuth = FirebaseAuth.getInstance();
+        mFirebaseDb = FirebaseDatabase.getInstance();
 
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
-        email_address = findViewById(R.id.email_address);
+        refUsers = mFirebaseDb.getReference()
+                .child("users");
 
-        btn_back = findViewById(R.id.btn_back);
+        etUsername = findViewById(R.id.username);
+        etPassword = findViewById(R.id.password);
+        etEmailAddress = findViewById(R.id.email_address);
 
-        btn_continue = findViewById(R.id.btn_continue);
-        btn_continue.setOnClickListener(new View.OnClickListener() {
+        btnBack = findViewById(R.id.btn_back);
+        btnContinue = findViewById(R.id.btn_continue);
+
+        btnContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 //ubah state menjadi loading
-                btn_continue.setEnabled(false);
-                btn_continue.setText("Loading...");
+                btnContinue.setEnabled(false);
+                btnContinue.setText("Loading...");
 
                 //mengambil username pada firebase
-                reference_username = FirebaseDatabase.getInstance().getReference()
-                        .child("Users").child(username.getText().toString());
 
-                reference_username.addListenerForSingleValueEvent(new ValueEventListener() {
+                String email = etEmailAddress.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        //Jika username tersedia
-                        if (dataSnapshot.exists()) {
-                            Toast.makeText(getApplicationContext(), "Username sudah tersedia", Toast.LENGTH_SHORT).show();
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
 
-                            //ubah state menjadi loading Active
-                            btn_continue.setEnabled(true);
-                            btn_continue.setText("CONTINUE");
-
+                            if (user != null) {
+                                saveUserToDatabase(user.getUid());
+                            }
                         } else {
-                            //menyimpan data kepada local storage (handphone)
-                            SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
-                            SharedPreferences.Editor editor = sharedPreferences.edit();
-                            editor.putString(username_key, username.getText().toString());
-                            editor.apply();
-
-                            //simpan kepada database
-                            reference = FirebaseDatabase.getInstance().getReference()
-                                    .child("Users").child(username.getText().toString());
-                            reference.addListenerForSingleValueEvent(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(DataSnapshot dataSnapshot) {
-                                    dataSnapshot.getRef().child("username").setValue(username.getText().toString());
-                                    dataSnapshot.getRef().child("password").setValue(password.getText().toString());
-                                    dataSnapshot.getRef().child("email_address").setValue(email_address.getText().toString());
-                                    dataSnapshot.getRef().child("user_balance").setValue(800);
-                                }
-
-                                @Override
-                                public void onCancelled(DatabaseError databaseError) {
-
-                                }
-                            });
-
-                            //berpindah activity
-                            Intent gotonextregister = new Intent(RegisterOneActivity.this, RegisterSuccessActivity.class);
-                            startActivity(gotonextregister);
+                            // If sign in fails, display a message to the user.
+                            Log.e("TAG", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            btnContinue.setEnabled(true);
+                            btnContinue.setText("CONTINUE");
                         }
-
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
                     }
                 });
-
-
             }
         });
 
-        btn_back.setOnClickListener(new View.OnClickListener() {
+        btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
             }
         });
 
+    }
+
+    private void saveUserToDatabase(final String userId) {
+        refUsers.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                dataSnapshot.getRef().child("username").setValue(etUsername.getText().toString());
+                dataSnapshot.getRef().child("password").setValue(etPassword.getText().toString());
+                dataSnapshot.getRef().child("email_address").setValue(etEmailAddress.getText().toString());
+                dataSnapshot.getRef().child("user_balance").setValue(800);
+
+                //Jika username tersedia
+                if (dataSnapshot.exists()) {
+                    Toast.makeText(getApplicationContext(), "Username sudah tersedia", Toast.LENGTH_SHORT).show();
+
+                    //ubah state menjadi loading Active
+                    btnContinue.setEnabled(true);
+                    btnContinue.setText("CONTINUE");
+
+                } else {
+                    //menyimpan data kepada local storage (handphone)
+                    SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString(USERNAME_KEY, etUsername.getText().toString());
+                    editor.apply();
+
+                    //simpan kepada database
+                }
+
+                //berpindah activity
+                Intent gotonextregister = new Intent(RegisterOneActivity.this, SuccessRegisterActivity.class);
+                startActivity(gotonextregister);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
 
